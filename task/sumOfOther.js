569ba1dcac5ea26a38d573adb67fe13306e98eeb
aborisenok <lesha_borisenok@mail.ru>
2018-10-28 12:45:34 +0300

    feat: solve task make and add mocha tests

--- task/sumOfOther.js
+++ task/sumOfOther.js
@@ -1,9 +1,7 @@
-function sumOfOther(arr){
-  let result = arr.map((elem, i, array) => {
-    return array.reduce((accum, current, j) => {
-      return i !== j ? accum + current : accum;
-    }, 0);
-  });
+function sumOfOther(arr) {
+  const result = arr.map((elem, i, array) => array.reduce((accum, current, j) => (
+    i !== j ? accum + current : accum
+  ), 0));
   return result;
 }
 
