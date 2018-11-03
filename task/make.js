38b687ddeaf30906d932cc0dab867287f80a0fa2
aborisenok <lesha_borisenok@mail.ru>
2018-11-03 14:52:59 +0300

    fix: fix the case when the make function doesn't have any number arguments

--- task/make.js
+++ task/make.js
@@ -3,7 +3,7 @@ function make(...params) {
   function foo(...args) {
     if (args[0] instanceof Function) {
       const func = args[0];
-      return values.reduce((accum, elem) => func(accum, elem));
+      return values.length ? values.reduce(func) : null;
     }
     values.push(...args);
     return foo;
